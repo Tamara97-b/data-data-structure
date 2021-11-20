c9927b07e294b9aa76609756e8531b722cef9af9
tamara97.b <tamarabassam1997@icloud.com>
2021-11-20 17:49:41 +0200

    done

--- isertionSort/lib/src/test/java/isertionSort/LibraryTest.java
+++ isertionSort/lib/src/test/java/isertionSort/LibraryTest.java
@@ -11,18 +11,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     MergeSort mergeSort = new MergeSort();
-        @Test
-        public void insertionSortTest() {
-            Library classUnderTest = new Library();
-            int[]arr={8,4,23,42,16,15};
-            int[]arr2={};
-            assertEquals("[4, 8, 15, 16, 23, 42]", Arrays.toString( classUnderTest.insertionSort(arr)));
-            assertEquals("[]", Arrays.toString( classUnderTest.insertionSort(arr2)));
+    @Test void ReverseInsertionSort() {
 
-
-        }
+        int[] ReverseSortedArr = {20, 18, 12, 8, 5, -2};
+        assertEquals("[-2, 5, 8, 12, 18, 20]", Arrays.toString(Library.insertionSort(ReverseSortedArr)));
+    }
+    @Test void FewUniquesInsertionSort() {
+        int[] FewUniquesArr = {5, 12, 7, 5, 5, 7};
+        assertEquals("[5, 5, 5, 7, 7, 12]", Arrays.toString(Library.insertionSort(FewUniquesArr)));
+    }
+    @Test void NearlyInsertionSort() {
+        int[]  NearlySortedArr = {2,3,5,7,13,11};
+        assertEquals("[2, 3, 5, 7, 11, 13]", Arrays.toString(Library.insertionSort(NearlySortedArr)));
+    }
     @Test
-    public void ReverseSorted(){
+    public void ReverseMergeSorted(){
         int[] array = {5,12,7,5,5,7};
         int n=array.length;
         int[] outputArray = mergeSort.merge(array,0,n-1);
@@ -31,7 +34,7 @@ class LibraryTest {
     }
 
     @Test
-    public void FewUniquesTest(){
+    public void FewUniquesMergeTest(){
         int[] array = {20,18,12,8,5,-2};
         int n=array.length;
         int[] outputArray = mergeSort.merge(array,0,n-1);
@@ -39,7 +42,7 @@ class LibraryTest {
         assertArrayEquals(expectedOutput,outputArray);
     }
     @Test
-    public void NearlySortedTest(){
+    public void NearlySortedMergeTest(){
         int[] array = {2,3,5,7,13,11};
         int n=array.length;
         int[] outputArray = mergeSort.merge(array,0,n-1);
